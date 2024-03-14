fbe2c0e99373b738452e74c795fe1dae9b25ad64
Ezra Basoco <subadaga@me.com>
2024-03-14 13:29:12 -0600

    Main Menu SKETCH

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // Load the level you want to play on button click
+        SceneManager.LoadScene("SampleScene"); // Replace with your level name
+    }
+
+    public void QuitGame()
+    {
+        // Quit the application on button click (works in builds, not editor)
+        Application.Quit();
+    }
+}
